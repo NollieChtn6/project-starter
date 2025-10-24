0f3a081cbe84fa67a3bed63401ee47f21e628f10
Noëllie CHATAIN SIX <noellie.chatain@hotmail.com>
2025-10-24 15:01:17 +0200

    Setup Vitest for backend + frontend
    - Removed Jest
    - Fixed non-null assertion issue
    - Added coverage support

--- apps/backend/jest.config.ts
+++
@@ -1,9 +0,0 @@
-import type { Config } from "jest";
-
-const config: Config = {
-  preset: "ts-jest",
-  testEnvironment: "node",
-  roots: ["<rootDir>/src", "<rootDir>/__tests__"],
-};
-
-export default config;
--- apps/backend/vitest.config.ts
+++ apps/backend/vitest.config.ts
@@ -1,17 +1,13 @@
 import { mergeConfig, defineConfig } from "vitest/config";
-import react from "@vitejs/plugin-react";
 import baseConfig from "../../vitest.config.base";
 
 export default mergeConfig(
   baseConfig,
   defineConfig({
-    plugins: [react()],
     test: {
       globals: true,
-      environment: "jsdom",
-      setupFiles: "./src/vitest.setup.ts",
-      include: ["src/**/*.test.{ts,tsx}"],
-      exclude: ["**/node_modules/**", "e2e", "**/e2e/**"],
+      environment: "node",
+      include: ["src/**/*.test.ts"],
       coverage: {
         reportsDirectory: "coverage/",
       },
